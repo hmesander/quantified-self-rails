dcf890473539f2ceb9fd1d248245135b0df6a059
hmesander <haleymesander@gmail.com>
2018-08-13 15:37:53 -0600

    Adds relationship specs for food model

--- spec/models/foods_spec.rb
+++ spec/models/foods_spec.rb
@@ -5,4 +5,9 @@ describe Food, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:calories) }
   end
+
+  describe 'Relationships' do
+    it { should have_many(:meal_foods) }
+    it { should have_many(:meals) }
+  end
 end
