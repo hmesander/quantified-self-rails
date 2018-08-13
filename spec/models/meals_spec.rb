87e20505607bc3f39aca8967fea9134aa0b710b5
hmesander <haleymesander@gmail.com>
2018-08-13 15:25:49 -0600

    Adds model validation specs for food

--- spec/models/meals_spec.rb
+++ spec/models/meals_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-describe Food, type: :model do
+describe Meal, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:calories) }
   end
 end
