ec8c8e5056567b73b81c1909990bf2513cd9e23c
hmesander <haleymesander@gmail.com>
2018-08-13 15:27:57 -0600

    Adds meal model and associated database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_212047) do
+ActiveRecord::Schema.define(version: 2018_08_13_212707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2018_08_13_212047) do
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
   end
 
 end
