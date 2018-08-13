fc882f13c409d149e6f5e6c354c8df9ce45ce1e6
hmesander <haleymesander@gmail.com>
2018-08-13 15:35:48 -0600

    Adds meal food model, associated database table, and relationships to meals and foods

--- spec/models/meal_foods_spec.rb
+++ spec/models/meal_foods_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe MealFood, type: :model do
   describe 'Relationships' do
-    it { should belong_to(:foods) }
-    it { should belong_to(:meals) }
+    it { should belong_to(:food) }
+    it { should belong_to(:meal) }
   end
 end
