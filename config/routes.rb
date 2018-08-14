22099e0292afc483b5e76746eb0997bb09dc058b
hmesander <haleymesander@gmail.com>
2018-08-14 11:16:56 -0600

    Updates welcome message to be more relevant

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
       resources :foods, only: [:index]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
