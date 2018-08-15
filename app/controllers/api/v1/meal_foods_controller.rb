f7d8db7d68b30b94d621e1d3a8be76cbd4fbf6bd
hmesander <haleymesander@gmail.com>
2018-08-15 12:59:59 -0600

    Refactors controllers to include a MealFoods controller

---
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::MealFoodsController < ApplicationController
+  def create
+    meal = Meal.find(params[:meal_id])
+    food = Food.find(params[:id])
+    meal_food = MealFood.create!(meal: meal, food: food)
+    render json: { message: "Successfully added #{food.name} to #{meal.name}" }, status: 201
+  end
+
+  def destroy
+    meal = Meal.find(params[:meal_id])
+    food = Food.find(params[:id])
+    meal_food = MealFood.find_by(meal: meal, food: food)
+    meal_food.destroy!
+    render json: { message: "Successfully deleted #{food.name} from #{meal.name}" }, status: 204
+  end
+end
--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -7,19 +7,4 @@ class Api::V1::MealsController < ApplicationController
     meal = Meal.find(params[:id])
     render json: meal
   end
-
-  def update
-    meal = Meal.find(params[:meal_id])
-    food = Food.find(params[:id])
-    meal_food = MealFood.create!(meal: meal, food: food)
-    render json: { message: "Successfully added #{food.name} to #{meal.name}" }, status: 201
-  end
-
-  def destroy
-    meal = Meal.find(params[:meal_id])
-    food = Food.find(params[:id])
-    meal_food = MealFood.find_by(meal: meal, food: food)
-    meal_food.destroy!
-    render json: { message: "Successfully deleted #{food.name} from #{meal.name}" }, status: 204
-  end
 end
