f03f7aca57bf0dc0e1b2909c07219d69d93da204
hmesander <haleymesander@gmail.com>
2018-08-14 21:41:43 -0600

    Adds specs and implementation of attempting to associate a meal and a food when either does not exist

--- spec/requests/api/v1/meal_foods/meal_foods_create_spec.rb
+++ spec/requests/api/v1/meal_foods/meal_foods_create_spec.rb
@@ -14,4 +14,24 @@ describe 'Foods API' do
       expect(returned['message']).to eq("Successfully added #{food.name} to #{breakfast.name}")
     end
   end
+
+  context 'User requests to associate a food with a meal and food does not exist' do
+    it 'send a message and 201 response' do
+      breakfast = Meal.create!(name: 'Breakfast')
+
+      post "/api/v1/meals/#{breakfast.id}/foods/1"
+
+      expect(response).to have_http_status(404)
+    end
+  end
+
+  context 'User requests to associate a food with a meal and meal does not exist' do
+    it 'send a message and 201 response' do
+      food = create(:food)
+
+      post "/api/v1/meals/1/foods/#{food.id}"
+
+      expect(response).to have_http_status(404)
+    end
+  end
 end
