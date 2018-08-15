08e76302d69b72560f96509ea1e315c7c1d2df0d
hmesander <haleymesander@gmail.com>
2018-08-14 19:13:45 -0600

    Implements happy path for creating a new food

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -14,4 +14,12 @@ class Api::V1::FoodsController < ApplicationController
       render status: 404
     end
   end
+
+  def create
+    food = Food.create!(
+      name: params['food']['name'],
+      calories: params['food']['calories']
+    )
+    render json: food
+  end
 end
