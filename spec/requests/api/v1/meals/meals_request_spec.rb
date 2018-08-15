2d279da1ed98e9232052e70c7f40c9f7bbd31c3b
hmesander <haleymesander@gmail.com>
2018-08-14 21:00:39 -0600

    Implements returning all meals and their associated foods

--- spec/requests/api/v1/meals/meals_request_spec.rb
+++ spec/requests/api/v1/meals/meals_request_spec.rb
@@ -5,18 +5,9 @@ describe 'Meals API' do
     it 'sends a list of meals and associated foods' do
       breakfast = Meal.create!(name: 'Breakfast')
       food1 = breakfast.foods.create!(name: 'Banana', calories: 150)
-      food2 = breakfast.foods.create!(name: 'Yogurt', calories: 550)
-      food3 = breakfast.foods.create!(name: 'Apple', calories: 220)
 
       snack = Meal.create!(name: 'Snack')
-      food4 = snack.foods.create!(name: 'Gum', calories: 40)
-      food5 = snack.foods.create!(name: 'Cheese', calories: 400)
-
-      lunch = Meal.create!(name: 'Lunch')
-      food6 = lunch.foods.create!(name: 'Burger', calories: 650)
-
-      dinner= Meal.create!(name: 'Dinner')
-      food7 = dinner.foods.create!(name: 'Steak', calories: 1650)
+      food2 = snack.foods.create!(name: 'Gum', calories: 400)
 
       get '/api/v1/meals'
 
@@ -26,28 +17,18 @@ describe 'Meals API' do
         {
           id: breakfast.id,
           name: breakfast.name,
-          foods: [food1, food2, food3]
+          foods: [id: food1.id, name: food1.name, calories: food1.calories]
         },
         {
           id: snack.id,
           name: snack.name,
-          foods: [food4, food5]
-        },
-        {
-          id: lunch.id,
-          name: lunch.name,
-          foods: [food6]
-        },
-        {
-          id: dinner.id,
-          name: dinner.name,
-          foods: [food7]
+          foods: [id: food2.id, name: food2.name, calories: food2.calories]
         }
       ]
 
       returned = JSON.parse(response.body)
 
-      expect(returned.length).to eq(4)
+      expect(returned.length).to eq(2)
       expect(response.body).to eq(expected.to_json)
       expect(returned).to_not include('created_at')
     end
