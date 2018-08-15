d77ea2404241cd9e15e623bb8d9e85ea0aad000d
hmesander <haleymesander@gmail.com>
2018-08-14 21:08:39 -0600

    Adds spec for requesting a single meal - happy path

--- spec/requests/api/v1/meals/meals_request_spec.rb
+++ spec/requests/api/v1/meals/meals_request_spec.rb
@@ -33,4 +33,32 @@ describe 'Meals API' do
       expect(returned).to_not include('created_at')
     end
   end
+
+  context 'User requests a single meal' do
+    it 'sends a single meal and its associated foods' do
+      breakfast = Meal.create!(name: 'Breakfast')
+      food1 = breakfast.foods.create!(name: 'Banana', calories: 150)
+
+      snack = Meal.create!(name: 'Snack')
+      food2 = snack.foods.create!(name: 'Gum', calories: 400)
+
+      get "/api/v1/meals/#{breakfast.id}/foods"
+
+      expect(response).to have_http_status(200)
+
+      expected = [
+        {
+          id: breakfast.id,
+          name: breakfast.name,
+          foods: [id: food1.id, name: food1.name, calories: food1.calories]
+        },
+      ]
+
+      returned = JSON.parse(response.body)
+
+      expect(returned.length).to eq(1)
+      expect(response.body).to eq(expected.to_json)
+      expect(returned).to_not include(snack.id)
+    end
+  end
 end
